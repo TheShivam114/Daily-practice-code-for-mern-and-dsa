8030b03be8826a6e93412672dcb2d872b8608706
TheShivam114 <shivam.ml114@gmail.com>
2025-10-24 15:16:31 +0530

    testing-2

--- Dsa Question Practice/day3.java
+++ Dsa Question Practice/day3.java
@@ -2,5 +2,6 @@ public class day3 {
      public static void main(String[] args){
          System.out.println("hi");
          System.out.println("by");
+           System.out.println("hey");
      }
 }
