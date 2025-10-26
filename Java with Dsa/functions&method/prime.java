fda9272e49ff266a51cb9423ec035505addc93bf
TheShivam114 <shivam.ml114@gmail.com>
2025-10-27 00:01:20 +0530

    function/methods

---
+++ Java with Dsa/functions&method/prime.java
@@ -0,0 +1,17 @@
+public class prime {
+    public static boolean isprime(int a){
+        if(a==2){
+            return true;
+        }
+        for(int i=2;i<(a-1);i++){   //for(int i=2;i<Math.sqrt(a);i++)
+            if(a%i==0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        System.out.println(isprime(12));
+    }
+}
