be087c73680159c662ec1daf9fddb0b46c3644b1
TheShivam114 <shivam.ml114@gmail.com>
2025-11-02 23:38:49 +0530

    day-12

---
+++ Dsa Question Practice/day12a.java
@@ -0,0 +1,23 @@
+//Write a Java program to print all pairs of elements in an array
+/*int arr[] = {2, 4, 6, 8, 10};
+(2, 4) (2, 6) (2, 8) (2, 10)
+(4, 6) (4, 8) (4, 10)
+(6, 8) (6, 10)
+(8, 10)
+Total pairs: 10
+ */
+public class day12a {
+    public static void pairs(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int crr=arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print("("+crr+","+arr[j]+")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int arr[]={2, 4, 6, 8, 10};
+        pairs(arr);
+    }
+}
