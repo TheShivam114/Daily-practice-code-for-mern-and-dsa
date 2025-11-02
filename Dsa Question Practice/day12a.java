227853b4d43d8bd21d5b95ede846e758358ca874
TheShivam114 <shivam.ml114@gmail.com>
2025-11-02 23:47:40 +0530

    day-12

--- Dsa Question Practice/day12a.java
+++ Dsa Question Practice/day12a.java
@@ -8,14 +8,18 @@ Total pairs: 10
  */
 public class day12a {
     public static void pairs(int arr[]){
+        int total=0;
         for(int i=0;i<arr.length;i++){
             int crr=arr[i];
             for(int j=i+1;j<arr.length;j++){
                 System.out.print("("+crr+","+arr[j]+")");
+                total++;
             }
             System.out.println();
         }
+        System.out.println("Total pairs: " +total);
     }
+    
     public static void main(String[] args){
         int arr[]={2, 4, 6, 8, 10};
         pairs(arr);
