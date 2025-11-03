d01582366b8d5cd4d92ff0dabc8f357d07c97c3f
TheShivam114 <shivam.ml114@gmail.com>
2025-11-03 22:30:17 +0530

    array

---
+++ Dsa Question Practice/day13a.java
@@ -0,0 +1,32 @@
+//Write a Java program to print all subarrays of a given array.
+/*int arr[] = {2, 4, 6};
+
+2
+2 4
+2 4 6
+4
+4 6
+6
+Total subarrays: 6
+
+ */
+public class day13a {
+    public static void subarray(int arr[]){
+     for(int i=0;i<arr.length;i++){
+        int start=i;
+        for(int j=i; j<arr.length;j++){
+        int end =j;
+        for(int k=start;k<=end;k++){
+            System.out.print(arr[k]+" ");
+        }
+        System.out.println();
+        }
+        System.out.println();
+     }
+    }
+    
+    public static void main(String[] args){
+        int arr[]={2, 4, 6, 8, 10};
+        subarray(arr);
+    }
+}
