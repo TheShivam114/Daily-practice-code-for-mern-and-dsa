0d58e1e00a24e33f2cd09c135f3a41f7cd9948b4
TheShivam114 <shivam.ml114@gmail.com>
2025-11-04 22:27:12 +0530

    improvement

--- Dsa Question Practice/day13a.java
+++ Dsa Question Practice/day13a.java
@@ -11,25 +11,27 @@ Total subarrays: 6
 
  */
 public class day13a {
-    public static void subarray(int arr[]){
-     for(int i=0;i<arr.length;i++){
-        int start=i;
-        for(int j=i; j<arr.length;j++){
-        int end =j;
-        for(int k=start;k<=end;k++){
-            
-            System.out.print(arr[k]+" " );
-            
+    public static void subarray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = i; j < arr.length; j++) {
+                int end = j;
+                int sum = 0;
+                for (int k = start; k <= end; k++) {
 
+                    System.out.print(arr[k] + " ");
+                    sum = sum + arr[k];
+
+                }
+                System.out.print(" sum = "+sum);
+                System.out.println();
+            }
+            System.out.println();
         }
-        System.out.println();
-        }
-        System.out.println();
-     }
     }
-    
-    public static void main(String[] args){
-        int arr[]={2, 4, 6, 8, 10};
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 6, 8, 10 };
         subarray(arr);
     }
 }
