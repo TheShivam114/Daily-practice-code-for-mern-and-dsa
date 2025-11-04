ae66ca5b33b66e4626972ea7d5969537589aa91e
TheShivam114 <shivam.ml114@gmail.com>
2025-11-04 23:58:55 +0530

    day 14

--- Dsa Question Practice/day14b.java
+++ Dsa Question Practice/day14b.java
@@ -1,4 +1,19 @@
+/*You are given an integer array arr[]of size n.Your task is to find the maximum sum of any
+ contiguous subarray(a subarray made of consecutive elements).
 
+ using prefix array
+ 
+ */
 public class day14b {
-    
+    public static void maxsub(int arr[]) {
+        int count;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+        
+    }
+}
+    public static void main(String[] args) {
+        int arr[] = { 1, -2, 6, -1, 3 };
+        maxsub(arr);
+    }
 }
