a05f9b60d1fbc2e542b6d0979ce3c2f97ee8286d
TheShivam114 <shivam.ml114@gmail.com>
2025-11-09 23:44:21 +0530

    2d array

---
+++ Dsa Question Practice/2d-day1.java
@@ -0,0 +1,22 @@
+/*You are given a 2D integer array matrix[][] of size n x m,
+where each row and each column is sorted in ascending order.
+
+Your task is to find whether a given target element x exists in the matrix.
+If it exists, print its position (row and column index), otherwise print “Not Found”.
+
+matrix = [
+    [10, 20, 30, 40],
+    [15, 25, 35, 45],
+    [27, 29, 37, 48],
+    [32, 33, 39, 50]
+]
+x = 29
+
+
+Element found at (2, 1)
+
+ */
+public class aeea2d_day1 {
+
+    
+}
