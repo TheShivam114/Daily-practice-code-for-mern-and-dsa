ae94db8d822ae3b626d20d7c9ac699f9c764e795
TheShivam114 <shivam.ml114@gmail.com>
2025-11-12 21:47:20 +0530

    best approach of diogonal sum

--- Dsa Question Practice/array2d_day3.java
+++ Dsa Question Practice/array2d_day3.java
@@ -14,23 +14,31 @@ The secondary diagonal consists of elements where row index + column index = ...
 public class array2d_day3 {
     public static int diagonal(int matrix[][]){
         int sum=0;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(i==j){
-                    sum += matrix[i][j];
-                }
-                else if(i+j==matrix.length-1){
-                    sum += matrix[i][j];
-                }
-            }
-        }
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(i==j){
+        //             sum += matrix[i][j];
+        //         }
+        //         else if(i+j==matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+
+        //best aproch
+       for(int i=0;i<matrix.length;i++){
+        
+            sum += matrix[i][i];
+           if(i!=matrix.length-1-i)
+           sum +=matrix[i] [matrix.length-1-i];
+       }
         return sum;
     }
     public static void main(String[] args){
         int arr[][] = { { 1, 2, 3, 4 },
-                { 5, 6, 7, 8 },
-                { 9, 10, 11, 12 },
-                { 13, 14, 15, 16 } };
+                        { 5, 6, 7, 8 },
+                        { 9, 10, 11, 12 },
+                        { 13, 14, 15, 16 } };
                 System.out.println(diagonal(arr));
         
     }
