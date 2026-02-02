c8a11fed5b8a2791efa7ecd87df966ef52954538
TheShivam114 <shivam.ml114@gmail.com>
2026-02-02 21:03:47 +0530

    js

---
+++ MERN stack/Front-End/javascript/day-26/script.js
@@ -0,0 +1,32 @@
+//js is Scripting Language
+//interpreter/compiler
+/*
+*intoduction to js.
+* why it is importance
+* what can it do for you
+* script tag
+* how to run js in browser
+* console... prompt alert
+* strings
+* slice template strings splite replace
+* includes
+* how many types of programming ar scripting language
+*/
+
+console.log("hay");
+console.error(" hi");
+console.warn("hi");
+
+// prompt("name?");
+// alert("hacked");
+
+//""write in double court
+"shivam bhi kaishe ho".slice(4,9);
+// `` ->template-String(template literal)
+"shivam bhi kaishe ho".split(4,9); //remove all a in the text.
+/*
+*.Split('a').join(' ')//it convert into array and joint make it in string again.
+*.replace all
+*.includes("a");
+ */
+
