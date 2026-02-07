65624e67c870439954c0196a24ea3b87dcb8288e
TheShivam114 <shivam.ml114@gmail.com>
2026-02-07 22:35:48 +0530

    java Script

--- MERN stack/Front-End/javascript/day-26/script.js
+++ MERN stack/Front-End/javascript/day-26/script.js
@@ -29,4 +29,38 @@ console.warn("hi");
 *.replace all
 *.includes("a");
  */
+//why var leaks outside block but let does't
+if(true){
+    var a=1;
+    let b=2;
 
+}
+console.log(a);//1
+console.log(b)//not correct refrence error
+
+// var  =>not respect if for any things it only respect function.
+
+//que=> why const allows changing object properties.
+
+const person={name:"harsh"};
+person.name="Sharma";//allowed
+person={};//not allowed
+
+
+/*section-2 datatype + type system
+/data types
+data is of many type but its devided into two types mainly
+ 1. primitive datatype 2. reference data type 
+1.primitives dataType:- Stored directly 
+
+  *String->test "hello",'sher'
+  *Numbler->nay number value 2,-99,3.4
+  *Boolean->true or false
+  *undefined->variable declared but not assigned
+             let x->x is not definded
+  *null->Intentional empty value.
+              let x=null;
+  *Symbol->unique identifier
+  *bidInt->vert large integers
+  1234567891011121314
+  
