f6c0ad00e244c546645a7ed88f81dd2d6e265779
TheShivam114 <shivam.ml114@gmail.com>
2026-02-25 23:52:29 +0530

    javascript

---
+++ MERN stack/Front-End/javascript/day-27/script.js
@@ -0,0 +1,49 @@
+/*
+{
+   var a=10;
+}
+
+console.log(a);   10
+  console.log("hey");
+  console.info("heyxyz");
+
+  both are same but in some broeser console.info message is showing i sign
+
+  i meazn info
+
+
+  console.tavle({name:"shivam"});
+
+
+  *promp alert
+
+  let nm = prompt("name?");
+  console.log(nm);
+
+  *alert(" ");
+
+  "1234a" ->string
+  12345a->this is string
+
+  * "" template strings
+
+  . "harsh 2+2"
+
+   => its print same as it is 'harsh 2+2
+     
+    . "harsh"+2+2
+      =>'harsh 4
+       
+
+      but if we used '' backtick then
+
+      *'harsh 2+2'   
+      =>'harsh 2+2'
+
+       if we calculate 'harsh ${2+2}'
+       =>'harsh 4'
+
+
+       ex- 'I am ${12/4}'
+       'I am 3'
+ */
