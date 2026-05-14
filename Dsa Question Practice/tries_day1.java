284064722a029d79cd37bff2feab8e0b859f580a
TheShivam114 <shivam.ml114@gmail.com>
2026-05-14 23:32:56 +0530

    trie-day1

---
+++ Dsa Question Practice/tries_day1.java
@@ -0,0 +1,51 @@
+/*
+Insert Strings into a Trie
+
+You are given a list of lowercase strings. Your task is to build a Trie (Prefix Tree) by inserting all the given strings one by one.
+
+A Trie is a tree-like data structure used to store strings efficiently. Each node represents a character, and words are formed by traversing from the root node to the end node.
+
+Your program only needs to perform the insert operation.
+
+Input Format
+First line contains an integer n — the number of strings.
+Next n lines contain one lowercase string each.
+Output Format
+
+Print:
+
+Inserted Successfully
+
+after inserting all strings into the Trie.
+
+Constraints
+1 <= n <= 1000
+1 <= length of string <= 100
+Strings contain only lowercase English letters.
+Example
+Input
+5
+apple
+app
+bat
+ball
+cat
+Output
+Inserted Successfully
+*/
+public class tries_day1 {
+    static class Node{
+        Node children[] =new Node[26];
+        boolean eow=false;
+
+        Node(){
+            for(int i=0;i<26;i++){
+                children[i]=null;
+            }
+        }
+    }
+    public static void main(String[] args){
+        String Word[]={"tre","a","there","their","any","thee"};
+        
+    }
+}
