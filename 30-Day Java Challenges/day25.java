07749278b160d4ef3f76104443c42db8f457f856
TheShivam114 <shivam.ml114@gmail.com>
2026-06-29 23:41:21 +0530

    hacker-rank-day25

---
+++ 30-Day Java Challenges/day25.java
@@ -0,0 +1,49 @@
+/**
+Today we're discussing Generics; be aware that not all languages support this construct, so fewer languages are enabled for this challenge. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Write a single generic function named printArray; this function must take an array of generic elements as a parameter (the exception to this is C++, which takes a vector). The locked Solution class in your editor tests your function.
+
+Note: You must use generics to solve this challenge. Do not write overloaded functions.
+
+Input Format
+
+The locked Solution class in your editor will pass different types of arrays to your printArray function.
+
+Constraints
+
+You must have exactly  function named printArray.
+Output Format
+
+Your printArray function should print each element of its generic array parameter on a new line.
+ */
+    import java.util.*;
+public class day25 {
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        int m = scanner.nextInt();
+        String[] stringArray = new String[m];
+        for (int i = 0; i < m; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        printArray(intArray);
+        printArray(stringArray);
+        scanner.close();
+    }
+}
