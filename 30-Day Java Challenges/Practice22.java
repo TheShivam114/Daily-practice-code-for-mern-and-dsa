966ac32f500e5fba8694ab7c34410e1900a72dbf
shivam kumar <shivam.ml114@gmail.com>
2026-08-08 23:53:53 +0530

    Add program to find second largest element in array
    Implemented a program to find the second largest element in an integer array without sorting. The solution handles duplicates and runs in O(n) time.

---
+++ 30-Day Java Challenges/Practice22.java
@@ -0,0 +1,11 @@
+Find the Second Largest Element
+Write a Java program to find the second largest element in an integer array without sorting the array.
+Input:
+arr = {10, 25, 5, 40, 30, 40}
+Expected Output:
+Second Largest: 30
+Rules:
+Don't use Arrays.sort().
+Try to solve it in O(n) time.
+Handle duplicate values correctly.
+Bonus: What will be the output for:
