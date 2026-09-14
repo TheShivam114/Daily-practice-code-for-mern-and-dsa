959fef6d52fe16ebb74fc92817c1a257d938b22d
TheShivam114 <shivam.ml114@gmail.com>
2026-09-14 23:28:05 +0530

    practice code java

---
+++ 30-Day Java Challenges/practice34.java
@@ -0,0 +1,65 @@
+/*
+ Search Insert Position
+Easy
+Topics
+premium lock icon
+Companies
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+*/
+class practice34{
+
+    public int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            // Target found
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Target is on the right side
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            // Target is on the left side
+            else {
+                right = mid - 1;
+            }
+        }
+
+        // left is the correct insertion position
+        return left;
+    }
+}
+    
